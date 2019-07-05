8543df1534a3376756edc2d57f34af15e4a3311a
Daniel Knauf <daniel-siegried.knauf@stud.uni-bamberg.de>
2019-07-05 13:07:52 +0200

    Build dungeonMap - add check for illegalArgument to prevent exception

--- src/main/java/model/utils/RandomNumberGenerator.java
+++ src/main/java/model/utils/RandomNumberGenerator.java
@@ -6,7 +6,9 @@ public class RandomNumberGenerator {
 
     private final Random randomize = new Random();
 
-    public int getRandomInteger(int bound){
-        return randomize.nextInt(bound);
+    public int getRandomInteger(int bound) {
+        return bound > 0
+                ? randomize.nextInt(bound)
+                : -1;
     }
 }
